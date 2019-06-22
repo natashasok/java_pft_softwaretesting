d27752815d9c2607f1de129a9008a6d638820868
natashasok <natashasok@users.noreply.github.com>
2019-06-22 15:02:59 +0300

    Добавлен класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,10 +13,18 @@ public class MyFirstProgram {
         Rectangle r= new Rectangle(4,6);
 
         System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(5,4);
+
+        System.out.println("Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" +  " = " + distance(p1, p2));
     }
         public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
 
     }
 
+    public static double distance( Point p1, Point p2){
+        return  Math.sqrt(Math.pow((p2.x -p1.x),2) + Math.pow((p2.y - p1.y),2));
+    }
 }
