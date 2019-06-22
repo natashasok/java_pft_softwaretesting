c21e506c94b9351d4fe2021eef1516f2715874ef
natashasok <natashasok@users.noreply.github.com>
2019-06-22 16:20:35 +0300

    Изменены тесты для класса point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -4,18 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTests {
+  Point p1 = new Point(1,1);
+  Point p2 = new Point(5,4);
 
   @Test
   public void testdistance(){
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(5,4);
     Assert.assertEquals(p1.distanceToPoint(p2),5.0);
   }
 
   @Test
   public void testdistance2(){
-    Point p1 = new Point(2,0);
-    Point p2 = new Point(5,4);
-    Assert.assertEquals(p1.distanceToPoint(p2),5.0);
+    Assert.assertEquals(p2.distanceToPoint(p1),5.0);
+  }
+  @Test
+  public void testdistance3(){
+    Assert.assertEquals(MyFirstProgram.distance(p1,p2),5.0);
   }
 }
