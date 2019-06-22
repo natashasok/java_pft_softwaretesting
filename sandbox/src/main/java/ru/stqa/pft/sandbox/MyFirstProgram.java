e3f475621f6caef09995bab81324678666301375
natashasok <natashasok@users.noreply.github.com>
2019-06-22 15:38:54 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -18,12 +18,15 @@ public class MyFirstProgram {
     Point p2 = new Point(5, 4);
 
 
-    System.out.println("Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + p1.distanceToPoint(p2));
+    System.out.println("Вычисление с помощью метода: Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + p1.distanceToPoint(p2));
+    System.out.println("Вычисление с помощью функции: Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + distance(p1,p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
 
   }
-
+  public static double distance( Point p1, Point p2){
+    return  Math.sqrt(Math.pow((p2.x -p1.x),2) + Math.pow((p2.y - p1.y),2));
+  }
 }
