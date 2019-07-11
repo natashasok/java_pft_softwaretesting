15b1b451107d96e341ad2da321b81285b0fea278
natashasok <nataffka.sokolova@gmail.com>
2019-07-11 13:35:28 +0300

    Лекция 3.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -13,13 +13,8 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
-    if (text != null) {
-      String existingText = wd.findElement(locator).getAttribute("value");
-      if (! text.equals(existingText)){
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
-      }
-    }
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
   }
 
   protected void click(By locator) {
