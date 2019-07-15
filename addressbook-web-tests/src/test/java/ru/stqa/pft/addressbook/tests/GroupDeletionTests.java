9a575e1a810cf6c98554078b0e048242ab2f237d
natashasok <nataffka.sokolova@gmail.com>
2019-07-15 15:46:55 +0300

    Добавлено сравнение списка до и после удаления группы (Лекция 4.6)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -20,6 +20,9 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().retutnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+      Assert.assertEquals(before, after);
     app.logout();
   }
 
