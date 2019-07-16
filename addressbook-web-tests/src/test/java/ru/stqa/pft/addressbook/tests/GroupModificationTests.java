0baf0900ae2843e04462e0cf747ce0726b3ed662
natashasok <nataffka.sokolova@gmail.com>
2019-07-16 19:59:36 +0300

    Доработан тест создания группы (Лекция 4.8)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -23,10 +23,11 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().retutnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() );
-    app.logout();
 
     before.remove(before.size() - 1);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    app.logout();
   }
 }
