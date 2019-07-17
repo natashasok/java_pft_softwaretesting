4c0c63b4e993edc38278c88e540f63793a877ec8
natashasok <nataffka.sokolova@gmail.com>
2019-07-17 18:49:03 +0300

    Доработан тест добавления контакта (лекция 4.9)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,21 +14,14 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     List<ContactData> before =app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Nataliia", "Sergeevna", "Sokolova", "natasha_sok", "ns", "N_company", "Yaroslavl", "70-80-90", "+7910-123-67-08", "99-70-54", "nsokolova_fax", "nsokolova@N_company.ru", "nsokolova2@N_company.ru", "1989", "18", "September", "test1");
+    ContactData contact = new ContactData("Nataliia2", "Sergeevna", "Sokolova", "natasha_sok", "ns", "N_company", "Yaroslavl", "70-80-90", "+7910-123-67-08", "99-70-54", "nsokolova_fax", "nsokolova@N_company.ru", "nsokolova2@N_company.ru", "1989", "18", "September", "test1");
     app.getContactHelper().initContactCreation();
     app.getContactHelper().createContact( contact, true);
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after =app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    before.add(contact);
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     app.logout();
