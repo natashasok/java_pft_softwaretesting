906c92efc72513330f1570ab7a4aeba649049e53
natashasok <nataffka.sokolova@gmail.com>
2019-07-17 19:02:06 +0300

    Сортировка списков в тесте создания и изменения группы (лекция 4.10)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -16,8 +16,9 @@ public class GroupData {
     this.id = id;
   }
 
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -50,18 +51,18 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    return Objects.hash(name);
   }
 
 }
