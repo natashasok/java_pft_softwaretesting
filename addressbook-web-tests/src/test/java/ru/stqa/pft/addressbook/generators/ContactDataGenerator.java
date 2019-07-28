00a5de3fe0d6083c1f4f0ba8cf4d2ef265e87616
natashasok <natashasok@users.noreply.github.com>
2019-07-28 21:30:38 +0300

    Анализ опций командной строки (лекция 6.3)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
-
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,15 +11,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
 
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+  @Parameter( names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter ( names = "-f", description = "Target file")
+  public String file;
+
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts,  new File (file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts){
