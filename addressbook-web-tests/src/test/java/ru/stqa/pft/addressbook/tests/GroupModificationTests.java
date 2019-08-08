519413cbb495bbbc5753c0acba51d4e4f445692f
natashasok <nataffka.sokolova@gmail.com>
2019-08-08 17:43:32 +0300

    Отключаемые проверки UI (Лекция 7.5)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -36,7 +36,7 @@ public class GroupModificationTests extends TestBase {
     assertThat(app.group().сount(), equalTo(before.size()));
     Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
-
+    verifyGroupListInUI();
   }
 
 }
