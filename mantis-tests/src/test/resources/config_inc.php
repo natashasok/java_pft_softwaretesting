afc5095e27da3472afe7bed023d90e5cc9391871
natashasok <nataffka.sokolova@gmail.com>
2019-08-20 18:57:00 +0300

    Добавлен класс MailHelper

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,3 +6,5 @@ $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_crypto_master_salt = '6qvN5DibNVXlhjeP7VrIqyfPUgaHZ14DMWklGKsriU8=';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
